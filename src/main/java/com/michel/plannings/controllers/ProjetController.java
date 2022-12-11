6953aebb49056f0a58391c7a8b682e8dca36ee1b
Michel <kintelalpz@gmail.com>
2022-12-11 09:48:24 +0100

    Mise en place mécanisme d'affichage des diagrammes de Gantt

--- src/main/java/com/michel/plannings/controllers/ProjetController.java
+++ src/main/java/com/michel/plannings/controllers/ProjetController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.michel.plannings.contants.Constants;
+import com.michel.plannings.models.GanttRow;
 import com.michel.plannings.models.ProjetAux;
 import com.michel.plannings.models.Utilisateur;
 import com.michel.plannings.models.UtilisateurAux;
@@ -186,6 +187,41 @@ public class ProjetController {
 		model.addAttribute("ressourcesDispos", ressourcesDisponibles);
 		
 		return Constants.testUser(utilisateur, Constants.AJOUTER_RESSOURCES_PROJET);
+		
+	}
+	
+	
+	@GetMapping("/projet/gantt/{id}")
+	public String projetGantt(@PathVariable(name = "id") Integer idProjet, Model model, HttpSession session) {
+		
+		Utilisateur utilisateur = userConnexion.obtenirUtilisateur(session, model);
+		String token = Constants.getToken(session);
+		
+		ProjetAux projet = microServicePlannnings.projetParId(token, idProjet);
+		model.addAttribute("projet", projet);
+		
+		GanttRow r1 = new GanttRow("t1", "Tache1", "xxx1 ", "2022-11-10", "2022-11-12", 0, 0, null);
+		GanttRow r2 = new GanttRow("t2", "Tache1", "xxx2 ","2022-11-12", "2022-11-16", 0, 0, null);
+		GanttRow r3 = new GanttRow("t3", "Tache3", "xxx3 ","2022-11-16", "2022-11-21", 0, 0, null);
+		GanttRow r4 = new GanttRow("t4", "Tache4", "xxx4 ","2022-11-21", "2022-11-25", 0, 0, null);
+		GanttRow r5 = new GanttRow("t5", "Tache5", "xxx ","2022-11-25", "2022-11-28", 0, 0, null);
+		GanttRow r6 = new GanttRow("t6", "Tache6", "xxx ","2022-11-28", "2022-12-3", 0, 0, null);
+		GanttRow r7 = new GanttRow("t7", "Tache7", "xxx ","2022-12-3", "2022-12-7", 0, 0, null);
+		GanttRow r8 = new GanttRow("t8", "Tache8", "xxx ","2022-12-7", "2022-12-12", 0, 0, null);
+		GanttRow r9 = new GanttRow("t9", "Tache9","xxx ", "2022-12-12", "2022-12-15", 0, 0, null);
+		GanttRow r10 = new GanttRow("t10", "Tache10", "xxx ","2022-12-15", "2022-12-17", 0, 0, null);
+		GanttRow r11 = new GanttRow("t11", "Tache11", "xxx ","2022-12-17", "2022-12-24", 0, 0, null);
+		GanttRow r12 = new GanttRow("t12", "Tache12", "xxx ","2022-12-24", "2022-12-28", 0, 0, null);
+		GanttRow r13 = new GanttRow("t13", "Tache13", "xxx ","2022-12-28", "2023-1-3", 0, 0, null);
+		GanttRow r14 = new GanttRow("t14", "Tache14", "xxx ","2023-1-3", "2023-1-29", 0, 0, null);
+		
+		GanttRow [] tab = {r1, r2, r3, r4, r5,r6,r7, r8, r9, r10, r11, r12, r13, r14};
+		String[] liste = {"AAAA", "BBBBB"};
+		model.addAttribute("tab", tab);
+		model.addAttribute("liste", liste);
+		//return Constants.testUser(utilisateur, "inlineJs");
+		return Constants.testUser(utilisateur, "gantt3");
+		//return Constants.testUser(utilisateur, "gantt");
 		
 	}
 }
---
+++ src/main/java/com/michel/plannings/models/GanttRow.java
@@ -0,0 +1,103 @@
+package com.michel.plannings.models;
+
+public class GanttRow {
+	
+	private String taskId;
+	private String taskName;
+	private String ressource;
+	private String startDate;
+	private String endDate;
+	private int duration;
+	private int percent;
+	private String dependencies;
+
+	public GanttRow() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+	public GanttRow(String taskId, String taskName, String ressource, String startDate, String endDate, int duration,
+			int percent, String dependencies) {
+		super();
+		this.taskId = taskId;
+		this.taskName = taskName;
+		this.ressource = ressource;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.duration = duration;
+		this.percent = percent;
+		this.dependencies = dependencies;
+	}
+
+
+
+	public String getTaskId() {
+		return taskId;
+	}
+
+	public void setTaskId(String taskId) {
+		this.taskId = taskId;
+	}
+
+	public String getTaskName() {
+		return taskName;
+	}
+
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+
+	public String getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+
+	public String getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	public int getPercent() {
+		return percent;
+	}
+
+	public void setPercent(int percent) {
+		this.percent = percent;
+	}
+
+	public String getDependencies() {
+		return dependencies;
+	}
+
+	public void setDependencies(String dependencies) {
+		this.dependencies = dependencies;
+	}
+
+	public String getRessource() {
+		return ressource;
+	}
+
+	public void setRessource(String ressource) {
+		this.ressource = ressource;
+	}
+	
+	
+
+
+}
